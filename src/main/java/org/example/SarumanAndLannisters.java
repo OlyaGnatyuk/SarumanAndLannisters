c398b4ae0d947ee1831ea2388500a5fd69fb33a3
OlyaGnatyuk <43353123+OlyaGnatyuk@users.noreply.github.com>
2022-11-01 14:40:29 +0300

    Initial commit

---
+++ src/main/java/org/example/SarumanAndLannisters.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class SarumanAndLannisters {
+    public static void main(String[] args) {
+        int lannistersSoldiersNumber = 80;
+        int sarumanSoldiersNumber = 50;
+        int totalSoldiersNumber = lannistersSoldiersNumber + sarumanSoldiersNumber;
+    }
+}
